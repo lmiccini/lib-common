395aef9e1a33fee0e51b4d7e41566a8f57f01113
Luca Miccini <lmiccini@redhat.com>
2025-06-16 09:05:44 +0200

    Helper function to create mtls certs

---
+++ modules/common/test/helpers/mtls.go
@@ -0,0 +1,36 @@
+/*
+Copyright 2023 Red Hat
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package helpers
+
+import (
+	"k8s.io/apimachinery/pkg/types"
+
+	corev1 "k8s.io/api/core/v1"
+)
+
+// CreateMTLSCertSecret creates a new secret with entries for the cert, key and a ca using fake data.
+//
+// Example usage:
+//
+//	certSecretName = types.NamespacedName{Name: "secretname", Namespace: namespace}
+//	s := th.CreateMTLSCertSecret(certSecretName)
+func (tc *TestHelper) CreateMTLSCertSecret(name types.NamespacedName) *corev1.Secret {
+	data := map[string][]byte{
+		"mtls-ca.crt": []byte("Zm9v"),
+		"mtls.crt":    []byte("Zm9v"),
+		"mtls.key":    []byte("Zm9v"),
+	}
+
+	return tc.CreateSecret(name, data)
+}
